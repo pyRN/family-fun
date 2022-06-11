306311e925182b37b0a4a7580870fd5e6891cc4c
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-11 17:01:12 -0500

    Styled landing page

--- src/components/LandingPage/LandingPageContainer.jsx
+++ src/components/LandingPage/LandingPageContainer.jsx
@@ -1,43 +1,40 @@
 import { useNavigate } from "react-router-dom";
 
 const LandingPageContainer = () => {
+  const fnNavigate = useNavigate();
   const aGames = [
     {
       sTitle: "Tic Tac Toe",
       sPath: "tic_tac_toe",
-      oImage: "https://via.placeholder.com/50",
     },
     {
       sTitle: "Paper, Rock, Scissors",
       sPath: "paper_rock_scissors",
-      oImage: "https://via.placeholder.com/50",
     },
     {
       sTitle: "Connect Four",
       sPath: "connect_four",
-      oImage: "https://via.placeholder.com/50",
     },
     {
       sTitle: "Simon Says",
       sPath: "simon_says",
-      oImage: "https://via.placeholder.com/50",
     },
   ];
   return (
-    <div className="container bg-primary">
-      <h1 className="text-secondary">Travel Fun</h1>
-      <div className="m-1">
+    <div className="container">
+      <div className="title-container">
+        <h1 className="text-center">Travel Fun</h1>
+      </div>
+
+      <div className="games-container">
         {aGames.map((oGame) => {
           return (
-            <div className="card" w-25>
-              <img
-                src={oGame.oImage}
-                className="card-img-top img-fluid"
-                alt={oGame.sTitle}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{oGame.sTitle}</h5>
-              </div>
+            <div
+              className="game-card"
+              key={oGame.sTitle}
+              onClick={() => fnNavigate(oGame.sPath)}
+            >
+              <h2>{oGame.sTitle}</h2>
             </div>
           );
         })}
