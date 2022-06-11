508910a2f97f8c10f939c35cd5d444b77a532716
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-11 17:00:05 -0500

    Removed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import {Route, BrowserRouter, Routes} from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
 
 //Componnents
-import LandingPage from "./components/LandingPage/LandingPageContainer"
-import TicTacToeContainer from "./components/TicTacToe/TicTacToeContainer"
-import PaperRockScissorsContainer from "./components/PaperRockScissors/PaperRockScissorsContainer"
-import ConnectFourContainer from "./components/ConnectFour/ConnectFourContainer"
-import SimonSaysContainer from "./components/SimonSays/SimonSaysContainer"
+import LandingPage from "./components/LandingPage/LandingPageContainer";
+import TicTacToeContainer from "./components/TicTacToe/TicTacToeContainer";
+import PaperRockScissorsContainer from "./components/PaperRockScissors/PaperRockScissorsContainer";
+import ConnectFourContainer from "./components/ConnectFour/ConnectFourContainer";
+import SimonSaysContainer from "./components/SimonSays/SimonSaysContainer";
 
 function App() {
   return (
@@ -16,7 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/tic_tac_toe" element={<TicTacToeContainer />} />
-          <Route path="/paper_rock_scissors" element={<PaperRockScissorsContainer />} />
+          <Route
+            path="/paper_rock_scissors"
+            element={<PaperRockScissorsContainer />}
+          />
           <Route path="/connect_four" element={<ConnectFourContainer />} />
           <Route path="/simon_says" element={<SimonSaysContainer />} />
         </Routes>
