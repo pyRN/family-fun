640690a6854b0dc068ec3a2c27f5256f28b2a43b
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-12 06:54:06 -0500

    Completed footer and styled element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 const Footer = () => {
   const fnNavigate = useNavigate();
   return (
-    <div>
-      <button className="btn back-btn" onClick={() => fnNavigate("/")}>
+    <footer>
+      <button className="back-btn" onClick={() => fnNavigate("/")}>
         Back
       </button>
-    </div>
+    </footer>
   );
 };
 
